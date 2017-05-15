bb1856a6a97ac013ba2f6fcc68e3ccbb81a53d71
skorpion <savinov_vu@inbox.ru>
2017-05-15 20:33:30 +0800

    create schema. create and populate table.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS person
+(
+  id        INTEGER NOT NULL AUTO_INCREMENT,
+  login     VARCHAR(255),
+  password  VARCHAR(255),
+  birthdate DATE,
+  PRIMARY KEY (id)
+);
+
+DELETE FROM person;
+
+INSERT INTO person (id, login, password, birthdate) VALUES
+  (1, 'name1', 'password1', '2011-01-11'),
+  (2, 'name2', 'password2', '2011-01-12'),
+  (3, 'name3', 'password3', '2011-01-13'),
+  (4, 'name4', 'password4', '2011-01-14'),
+  (5, 'Имя5', 'password5', '2011-01-15'),
+  (6, 'name6', 'password6', '2011-01-16');
