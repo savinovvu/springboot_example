320d2d103243f053e907b61773ed55983a647b16
skorpion <savinov_vu@inbox.ru>
2017-05-15 23:24:35 +0800

    add util classes

---
+++ src/main/java/ru/inbox/savinov_vu/util/json/JsonDateDeserializer.java
@@ -0,0 +1,17 @@
+package ru.inbox.savinov_vu.util.json;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class JsonDateDeserializer extends JsonDeserializer<LocalDate> {
+    @Override
+    public LocalDate deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return LocalDate.parse(p.getText(), dtf);
+    }
+}
---
+++ src/main/java/ru/inbox/savinov_vu/util/json/JsonDateSerializer.java
@@ -0,0 +1,22 @@
+package ru.inbox.savinov_vu.util.json;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
+@Component
+public class JsonDateSerializer extends JsonSerializer<LocalDate> {
+    private static final DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    @Override
+    public final void serialize(LocalDate value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
+        String isoDate = format.format(value);
+        jgen.writeString(isoDate);
+    }
+}
